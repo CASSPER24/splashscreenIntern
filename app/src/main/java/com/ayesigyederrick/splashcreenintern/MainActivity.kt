0413cdb7a242269793e4397c0a305ae422bbb673
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2023-03-17 12:21:26 +0300

    We have added webview for kyu portal

--- app/src/main/java/com/ayesigyederrick/splashcreenintern/MainActivity.kt
+++ app/src/main/java/com/ayesigyederrick/splashcreenintern/MainActivity.kt
@@ -2,10 +2,30 @@ package com.ayesigyederrick.splashcreenintern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+    val webView: WebView ? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "Kyu Portal"
+
+        val webview = findViewById<WebView>(R.id.WebView)
+        webview.webViewClient = WebViewClient()
+
+        webview.loadUrl("https://myportal.kyu.ac.ug/")
+        val webSettings = webview.settings
+        webSettings.javaScriptEnabled = true
+
     }
+    override fun onBackPressed() {
+        if(webView?.canGoBack() == true){
+            webView?.goBack()
+        }
+        super.onBackPressed()
+    }
+
 }
